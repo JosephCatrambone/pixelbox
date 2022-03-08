89947fa8875eb59fefbb947ff4fb2c6746d3f918
JosephCatrambone <jo.jcat@gmail.com>
2022-03-08 12:02:38 -0800

    Revert "Revert "Small update.  Use labeled classes to train." in favor of making this a separate file."  Decided I like it this way.
    This reverts commit 730f066fb5f6e7dce98e6882ad5ff5226f2c2a52.

--- resources/train.py
+++ resources/train.py
@@ -6,105 +6,115 @@ from tqdm import tqdm
 
 ENCODER_INPUT_WIDTH = 255
 ENCODER_INPUT_HEIGHT = 255
+LATENT_SPACE_SIZE = 8
+LEARNING_RATE = 1e-4
+EPOCHS = 100
+BATCH_SIZE = 16  # Remember that we get this^2 tensor when computing.
 
-device = torch.device("cuda:0")
+DEVICE = torch.device("cuda")
 #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Define our mutations to perform on the network.
-training_preprocessing = torchvision.transforms.Compose([
-	torchvision.transforms.Resize(300),  # 44 pixels of play to reshuffle.
+corruptions = torchvision.transforms.Compose([
+	torchvision.transforms.RandomVerticalFlip(),
+	torchvision.transforms.RandomHorizontalFlip(),
+	torchvision.transforms.RandomInvert(),
+	torchvision.transforms.RandomRotation(45),
+	torchvision.transforms.ColorJitter(),
+	torchvision.transforms.Resize(int(ENCODER_INPUT_WIDTH*1.2)),  # 44 pixels of play to reshuffle.
 	#torchvision.transforms.CenterCrop
 	torchvision.transforms.RandomResizedCrop((ENCODER_INPUT_WIDTH, ENCODER_INPUT_HEIGHT)),  # This might be backwards.
 	torchvision.transforms.ToTensor(),
 ])
 
-release_preprocessing = torchvision.transforms.Compose([
-	torchvision.transforms.Resize((ENCODER_INPUT_WIDTH, ENCODER_INPUT_HEIGHT)),
-	torchvision.transforms.ToTensor(),
-])
-
-corruptions = torch.nn.Sequential(
-	torchvision.transforms.ColorJitter(),
-	torchvision.transforms.RandomHorizontalFlip(),
-	torchvision.transforms.RandomVerticalFlip(),
-	#torchvision.transforms.RandomGrayscale(),
-	#torchvision.transforms.ToTensor(),
-	#torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-)
-#scripted_transforms = torch.jit.script(corruptions)
-
-# Load data.
-BATCH_SIZE = 4
-#data_dir = './data/'
-data_dir = "/home/joseph/Pictures/"
-image_dataset = torchvision.datasets.ImageFolder(data_dir, training_preprocessing)
-dataloader = torch.utils.data.DataLoader(image_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
-dataset_size = len(image_dataset)
-class_names = image_dataset.classes
 
 # Define our model.
-encoder = ImageEncoder()
+def build_model(latent_space: int):
+	model = torch.nn.Sequential(
+		torch.nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1),
+		torch.nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1),
+		torch.nn.LeakyReLU(inplace=True),
+		torch.nn.AvgPool2d(3),
+		torch.nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1),
+		torch.nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1),
+		torch.nn.LeakyReLU(inplace=True),
+		torch.nn.AvgPool2d(3),
+		torch.nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, stride=1, padding=1),
+		torch.nn.Conv2d(in_channels=1024, out_channels=128, kernel_size=3, stride=1, padding=1),
+		torch.nn.LeakyReLU(inplace=True),
+		torch.nn.AvgPool2d(3),
+		torch.nn.Flatten(),
+		torch.nn.Linear(in_features=10368, out_features=1024),
+		torch.nn.Linear(in_features=1024, out_features=latent_space)
+	)
+	return model
 
-class TwinNetwork(torch.nn.Module):
-	def __init__(self, encoder):
-		super().__init__()
-		self.encoder = encoder
-		self.distance = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
 
-	def forward(self, x_1, x_2):
-		x_1 = self.encoder(x_1)
-		x_2 = self.encoder(x_2)
-		return self.distance(x_1, x_2)
-		#return torch.abs(x_1 - x_2).sum()
+# Set up
+def train(training_data_directory, model=None):
+	if model is None:
+		model = build_model(10).to(DEVICE)
 
-net = TwinNetwork(encoder).to(device)
+	# Brace for run...
+	loss_fn = torch.nn.CosineEmbeddingLoss()
+	optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+	dataset = torchvision.datasets.ImageFolder(training_data_directory, transform=corruptions)
+	dataset_loader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=4, pin_memory=True)
 
-# Train the fuck out of it.
-loss_fn = torch.nn.MSELoss() # Not BCELoss.
-optimizer = torch.optim.SGD(net.parameters(), lr=0.01, momentum=0.7)
+	# Training loop:
+	for epoch_idx in range(EPOCHS):
+		dataloop = tqdm(dataset_loader)
+		total_epoch_loss = 0.0
+		for batch_idx, (data, targets) in enumerate(dataloop):
+			data = data.to(device=DEVICE)
+			optimizer.zero_grad()
 
-losses = list()
+			# Forward
+			embeddings = model(data)
 
-for epoch in tqdm(range(500)):
-	batch_losses = list()
-	for batch in dataloader:
-		inputs, _labels = batch
-		if inputs.shape[0] < 4:
-			continue
-		inputs = inputs.to(device)
-		# We ignore labels and, for some examples, swap two of the images to mark them as false.
-		x1 = corruptions(inputs)
-		x2 = corruptions(inputs) # torch.zeros_like(x1)
+			# One embedding gives us n*(n-1) pairs of datapoints.
+			# We rely on the batch being shuffled and having some of each class, but if the entire batch is unlucky
+			# and we have all one class, it will be okay.
+			# left takes [1, 2, 3, 4] and goes to [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4]
+			# right takes [1, 2, 3, 4] and goes to [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+			left = torch.repeat_interleave(embeddings, embeddings.shape[0], axis=0)
+			right = embeddings.repeat(embeddings.shape[0], 1)
+			truth = list()
+			for label_left in targets:
+				for label_right in targets:
+					truth.append(1.0 if label_left == label_right else -1.0)
+			truth = torch.tensor(truth).to(DEVICE)
 
-		# Mutate x2 and swap some to make the negative case.
-		# When we swap two entries, mark them as zero, 'cause they don't match.
-		indices = torch.LongTensor([idx for idx in range(x2.shape[0])])
-		indices[0] = 1
-		indices[1] = 0
-		x2[indices] = x2
-		y = numpy.ones(x2.shape[0], dtype=numpy.float32)
-		y[0] = 0
-		y[1] = 0
-		y = torch.Tensor(y).to(device)
+			# Embedding pairs are 1 if they're the same and -1 if they're not.
+			# We match up embeddings based on their classes.
+			loss = loss_fn(left, right, truth)
 
-		optimizer.zero_grad()
-		out = net(x1, x2)
-		loss = loss_fn(out, y)
-		loss.backward()
-		optimizer.step()
-		batch_losses.append(loss.item())
-		#writer.add_scalar('Loss/train', loss.item(), n_iter)
-	print("Max/Min/Avg")
-	print(max(batch_losses), end="\t\t")
-	print(min(batch_losses), end="\t\t")
-	print(sum(batch_losses)/len(batch_losses), end="\t\t")
-	losses.append(sum(batch_losses)/len(batch_losses))
-	if epoch % 10 == 0:
-		torch.save(encoder.state_dict(), f"enc_epoch_{epoch}.pt")
-# Build final model:
-device = torch.device("cpu")
-encoder_cpu = encoder.to(device)
-example = torch.rand(1, 3, ImageEncoder.INPUT_SIZE, ImageEncoder.INPUT_SIZE).to(device)
-torch.onnx.export(encoder_cpu, example, "encoder_cpu.onnx", example_outputs=encoder_cpu(example), opset_version=11)
-traced_script_module = torch.jit.trace(encoder_cpu, example)
-traced_script_module.save("encoder_cpu.pt")
+			# Backward
+			loss.backward()
+			optimizer.step()
+
+			# Log status.
+			total_epoch_loss += loss.item()
+
+		print(f"Total epoch loss: {total_epoch_loss}")
+		torch.save(model.state_dict(), f"checkpoints/checkpoint_{epoch_idx}")
+	torch.save(model, "result_model.pt")
+
+
+def finalize(encoder):
+	# Build final model:
+	device = torch.device("cpu")
+	encoder_cpu = encoder.to(device)
+	example = torch.rand(1, 3, ENCODER_INPUT_HEIGHT, ENCODER_INPUT_WIDTH).to(device)
+	torch.onnx.export(encoder_cpu, example, "encoder_cpu.onnx", example_outputs=encoder_cpu(example), opset_version=11)
+	traced_script_module = torch.jit.trace(encoder_cpu, example)
+	traced_script_module.save("encoder_cpu.pt")
+
+
+def main():
+	model = build_model(LATENT_SPACE_SIZE)
+	train("/home/joseph/Pictures", model)
+	finalize(model)
+
+if __name__ == "__main__":
+	main()
